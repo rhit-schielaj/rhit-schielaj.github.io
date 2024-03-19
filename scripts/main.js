6edef6342b8a51fecd3350c29ed8782c5028680b
rhit-schielaj <157640971+rhit-schielaj@users.noreply.github.com>
2024-03-18 21:23:44 -0400

    final edits

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("stickyHeader");
+
+
+var sticky = header.clientHeight;
+
+function myFunction() {
+    if (window.scrollY > sticky) {
+      header.classList.add("sticky");
+    } 
+    else {
+      header.classList.remove("sticky");
+    }
+  } 
