4a0ecf6e72b1254d638970fa94db76f947aeda52
rhit-schielaj <157640971+rhit-schielaj@users.noreply.github.com>
2024-04-06 18:14:15 -0400

    fixed sticky

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("stickyHeader");
+// var header = document.getElementById("stickyHeader");
 
 
-var sticky = header.clientHeight;
+// var sticky = header.clientHeight;
 
-function myFunction() {
-    if (window.scrollY > sticky) {
-      header.classList.add("sticky");
-    } 
-    else {
-      header.classList.remove("sticky");
-    }
-  } 
+// function myFunction() {
+//     if (window.scrollY > sticky) {
+//       header.classList.add("sticky");
+//     } 
+//     else {
+//       header.classList.remove("sticky");
+//     }
+//   } 
